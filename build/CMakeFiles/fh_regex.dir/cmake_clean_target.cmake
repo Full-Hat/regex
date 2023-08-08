65c39ce58ef10a9b5dde0c11ffc1beda0b2ee2d4
Full-Hat <68519677+Full-Hat@users.noreply.github.com>
2023-08-08 09:36:05 -0700

    Delete build directory

--- build/CMakeFiles/fh_regex.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libfh_regex.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/full-hat/Documents/MyProjects/regex
-# Build directory: /Users/full-hat/Documents/MyProjects/regex/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
